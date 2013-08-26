8bc6e93be2de499ff0284db97b56638d97c2e2dc
Ryan Barrett <git@ryanb.org>
2013-08-25 22:31:35 -0700

    start on instagram

---
+++ instagram.py
@@ -0,0 +1,317 @@
+#!/usr/bin/python
+"""Instagram source class.
+"""
+
+__author__ = ['Ryan Barrett <activitystreams@ryanb.org>']
+
+import cgi
+import datetime
+import itertools
+try:
+  import json
+except ImportError:
+  import simplejson as json
+import re
+import urllib
+import urlparse
+
+import appengine_config
+import source
+from webutil import util
+
+OAUTH_SCOPES = 'read_stream'
+
+API_OBJECT_URL = 'https://graph.instagram.com/%s'
+API_SELF_POSTS_URL = 'https://graph.instagram.com/%s/posts?offset=%d&limit=%d'
+# this an old, out of date version of the actual news feed. sigh. :/
+# "Note: /me/home retrieves an outdated view of the News Feed. This is currently
+# a known issue and we don't have any near term plans to bring them back up into
+# parity."
+# https://developers.instagram.com/docs/reference/api/#searching
+API_FEED_URL = 'https://graph.instagram.com/%s/home?offset=%d&limit=%d'
+
+# maps instagram graph api object types to ActivityStreams objectType.
+OBJECT_TYPES = {
+  'application': 'application',
+  'event': 'event',
+  'group': 'group',
+  'link': 'article',
+  'location': 'place',
+  'page': 'page',
+  'photo': 'photo',
+  'post': 'note',
+  'user': 'person',
+  }
+
+
+class Instagram(source.Source):
+  """Implements the ActivityStreams API for Instagram.
+  """
+
+  DOMAIN = 'instagram.com'
+  FRONT_PAGE_TEMPLATE = 'templates/instagram_index.html'
+  AUTH_URL = '&'.join((
+      ('http://localhost:8000/dialog/oauth/?'
+       if appengine_config.MOCKINSTAGRAM else
+       'https://www.instagram.com/dialog/oauth/?'),
+      'scope=%s' % OAUTH_SCOPES,
+      'client_id=%s' % appengine_config.INSTAGRAM_APP_ID,
+      # firefox and chrome preserve the URL fragment on redirects (e.g. from
+      # http to https), but IE (6 and 8) don't, so i can't just hard-code http
+      # as the scheme here, i need to actually specify the right scheme.
+      'redirect_uri=%s://%s/' % (appengine_config.SCHEME, appengine_config.HOST),
+      'response_type=token',
+      ))
+
+  def get_actor(self, user_id=None):
+    """Returns a user as a JSON ActivityStreams actor dict.
+
+    Args:
+      user_id: string id or username. Defaults to 'me', ie the current user.
+    """
+    if user_id is None:
+      user_id = 'me'
+    return self.user_to_actor(json.loads(self.urlfetch(API_OBJECT_URL % user_id)))
+
+  def get_activities(self, user_id=None, group_id=None, app_id=None,
+                     activity_id=None, start_index=0, count=0):
+    """Returns a (Python) list of ActivityStreams activities to be JSON-encoded.
+
+    See method docstring in source.py for details.
+
+    OAuth credentials must be provided in the access_token query parameter.
+    """
+    if user_id is None:
+      user_id = 'me'
+
+    if activity_id:
+      posts = [json.loads(self.urlfetch(API_OBJECT_URL % activity_id))]
+      if posts == [False]:  # FB returns false for "not found"
+        posts = []
+      total_count = len(posts)
+    else:
+      url = API_SELF_POSTS_URL if group_id == source.SELF else API_FEED_URL
+      url = url % (user_id, start_index, count)
+      posts = json.loads(self.urlfetch(url)).get('data', [])
+      total_count = None
+
+    return total_count, [self.post_to_activity(p) for p in posts]
+
+  def urlfetch(self, url, **kwargs):
+    """Wraps Source.urlfetch() and passes through the access_token query param.
+    """
+    access_token = self.handler.request.get('access_token')
+    if access_token:
+      parsed = list(urlparse.urlparse(url))
+      # query params are in index 4
+      # TODO: when this is on python 2.7, switch to urlparse.parse_qsl
+      params = cgi.parse_qsl(parsed[4]) + [('access_token', access_token)]
+      parsed[4] = urllib.urlencode(params)
+      url = urlparse.urlunparse(parsed)
+
+    return util.urlfetch(url, **kwargs)
+
+  def post_to_activity(self, post):
+    """Converts a post to an activity.
+
+    Args:
+      post: dict, a decoded JSON post
+
+    Returns:
+      an ActivityStreams activity dict, ready to be JSON-encoded
+    """
+    object = self.post_to_object(post)
+    activity = {
+      'verb': 'post',
+      'published': object.get('published'),
+      'updated': object.get('updated'),
+      'id': object.get('id'),
+      'url': object.get('url'),
+      'actor': object.get('author'),
+      'object': object,
+      }
+
+    application = post.get('application')
+    if application:
+      activity['generator'] = {
+        'displayName': application.get('name'),
+        'id': util.tag_uri(self.DOMAIN, application.get('id')),
+        }
+
+    self.postprocess_activity(activity)
+    return util.trim_nulls(activity)
+
+  def post_to_object(self, post):
+    """Converts a post to an object.
+
+    Args:
+      post: dict, a decoded JSON post
+
+    Returns:
+      an ActivityStreams object dict, ready to be JSON-encoded
+    """
+    object = {}
+
+    id = post.get('id')
+    if not id:
+      return {}
+
+    post_type = post.get('type')
+    status_type = post.get('status_type')
+    url = 'http://instagram.com/' + id.replace('_', '/posts/')
+    picture = post.get('picture')
+
+    object = {
+      'id': util.tag_uri(self.DOMAIN, str(id)),
+      'objectType': OBJECT_TYPES.get(post_type, 'note'),
+      'published': util.maybe_iso8601_to_rfc3339(post.get('created_time')),
+      'updated': util.maybe_iso8601_to_rfc3339(post.get('updated_time')),
+      'author': self.user_to_actor(post.get('from')),
+      'content': post.get('message'),
+      # FB post ids are of the form USERID_POSTID
+      'url': url,
+      'image': {'url': picture},
+      }
+
+    # tags
+    tags = itertools.chain(post.get('to', {}).get('data', []),
+                           post.get('with_tags', {}).get('data', []),
+                           *post.get('message_tags', {}).values())
+    object['tags'] = [{
+          'objectType': OBJECT_TYPES.get(t.get('type'), 'person'),
+          'id': util.tag_uri(self.DOMAIN, t.get('id')),
+          'url': 'http://instagram.com/%s' % t.get('id'),
+          'displayName': t.get('name'),
+          'startIndex': t.get('offset'),
+          'length': t.get('length'),
+          } for t in tags]
+
+    # is there an attachment? prefer to represent it as a picture (ie image
+    # object), but if not, fall back to a link.
+    link = post.get('link')
+    att = {
+        'url': link if link else url,
+        'image': {'url': picture},
+        'displayName': post.get('name'),
+        'summary': post.get('caption'),
+        'content': post.get('description'),
+        }
+
+    if (picture and picture.endswith('_s.jpg') and
+        (post_type == 'photo' or status_type == 'added_photos')):
+      # a picture the user posted. get a larger size.
+      att.update({
+          'objectType': 'image',
+          'image': {'url': picture[:-6] + '_o.jpg'},
+          })
+      object['attachments'] = [att]
+    elif link:
+      att['objectType'] = 'article'
+      object['attachments'] = [att]
+
+    # location
+    place = post.get('place')
+    if place:
+      id = place.get('id')
+      object['location'] = {
+        'displayName': place.get('name'),
+        'id': id,
+        'url': 'http://instagram.com/' + id,
+        }
+      location = place.get('location', None)
+      if isinstance(location, dict):
+        lat = location.get('latitude')
+        lon = location.get('longitude')
+        if lat and lon:
+          object['location'].update({
+              'latitude': lat,
+              'longitude': lon,
+              # ISO 6709 location string. details: http://en.wikipedia.org/wiki/ISO_6709
+              'position': '%+f%+f/' % (lat, lon),
+              })
+
+    # comments go in the replies field, according to the "Responses for
+    # Activity Streams" extension spec:
+    # http://activitystrea.ms/specs/json/replies/1.0/
+    comments = post.get('comments', {}).get('data')
+    if comments:
+      items = [self.comment_to_object(c) for c in comments]
+      object['replies'] = {
+        'items': items,
+        'totalItems': len(items),
+        }
+
+    return util.trim_nulls(object)
+
+  COMMENT_ID_RE = re.compile('(\d+)_(\d+)_(\d+)')
+
+  def comment_to_object(self, comment):
+    """Converts a comment to an object.
+
+    Args:
+      comment: dict, a decoded JSON comment
+
+    Returns:
+      an ActivityStreams object dict, ready to be JSON-encoded
+    """
+    # the message_tags field is different in comment vs post. in post, it's a
+    # dict of lists, in comment it's just a list. so, convert it to post style
+    # here before running post_to_object().
+    comment = dict(comment)
+    comment['message_tags'] = {'1': comment.get('message_tags', [])}
+
+    object = self.post_to_object(comment)
+    if not object:
+      return object
+
+    object['objectType'] = 'comment'
+
+    match = self.COMMENT_ID_RE.match(comment.get('id', ''))
+    if match:
+      object['url'] = 'http://instagram.com/%s/posts/%s?comment_id=%s' % match.groups()
+      object['inReplyTo'] = {
+        'id': util.tag_uri(self.DOMAIN, '%s_%s' % match.group(1, 2)),
+        }
+
+    return object
+
+  def user_to_actor(self, user):
+    """Converts a user to an actor.
+
+    Args:
+      user: dict, a decoded JSON Instagram user
+
+    Returns:
+      an ActivityStreams actor dict, ready to be JSON-encoded
+    """
+    if not user:
+      return {}
+
+    id = user.get('id')
+    username = user.get('username')
+    handle = username or id
+    if not handle:
+      return {}
+
+    url = user.get('link')
+    if not url:
+      url = 'http://instagram.com/' + handle
+
+    # instagram implements this as a 302 redirect
+    image_url = 'http://graph.instagram.com/%s/picture?type=large' % handle
+    actor = {
+      'displayName': user.get('name'),
+      'image': {'url': image_url},
+      'id': util.tag_uri(self.DOMAIN, handle),
+      'updated': util.maybe_iso8601_to_rfc3339(user.get('updated_time')),
+      'url': url,
+      'username': username,
+      'description': user.get('bio'),
+      }
+
+    location = user.get('location')
+    if location:
+      actor['location'] = {'id': location.get('id'),
+                           'displayName': location.get('name')}
+
+    return util.trim_nulls(actor)
---
+++ instagram_test.py
@@ -0,0 +1,490 @@
+#!/usr/bin/python
+# -*- eval: (progn (make-local-variable 'before-save-hook) (remove-hook 'before-save-hook 'delete-trailing-whitespace-in-some-modes t)) -*-
+#
+# (the above line is an Emacs file local variable that says *not* to delete
+# trailing whitespace, since some of it in test data is meaningful.)
+"""Unit tests for instagram.py.
+"""
+
+__author__ = ['Ryan Barrett <activitystreams@ryanb.org>']
+
+try:
+  import json
+except ImportError:
+  import simplejson as json
+import mox
+import urllib
+import urlparse
+from webutil import webapp2
+
+import instagram
+import source
+from webutil import testutil
+from webutil import util
+
+
+# test data
+def tag_uri(name):
+  return util.tag_uri('instagram.com', name)
+
+USER = {  # Instagram
+  'id': '212038',
+  'name': 'Ryan Barrett',
+  'link': 'http://www.instagram.com/snarfed.org',
+  'username': 'snarfed.org',
+  'location': {'id': '123', 'name': 'San Francisco, California'},
+  'updated_time': '2012-01-06T02:11:04+0000',
+  'bio': 'something about me',
+  }
+ACTOR = {  # ActivityStreams
+  'displayName': 'Ryan Barrett',
+  'image': {'url': 'http://graph.instagram.com/snarfed.org/picture?type=large'},
+  'id': tag_uri('snarfed.org'),
+  'updated': '2012-01-06T02:11:04+00:00',
+  'url': 'http://www.instagram.com/snarfed.org',
+  'username': 'snarfed.org',
+  'description': 'something about me',
+  'location': {'id': '123', 'displayName': 'San Francisco, California'},
+  }
+COMMENTS = [{  # Instagram
+    'id': '212038_547822715231468_6796480',
+    'from': {
+      'name': 'Ryan Barrett',
+      'id': '212038'
+      },
+    'message': 'cc Sam G, Michael M',
+    'message_tags': [{
+        'id': '221330',
+        'name': 'Sam G',
+        'type': 'user',
+        'offset': 3,
+        'length': 5,
+        }, {
+        'id': '695687650',
+        'name': 'Michael Mandel',
+        'type': 'user',
+        'offset': 10,
+        'length': 9,
+        }],
+    'created_time': '2012-12-05T00:58:26+0000',
+    }, {
+    'id': '212038_124561947600007_672819',
+    'from': {
+      'name': 'Ron Ald',
+      'id': '513046677'
+      },
+    'message': 'Foo bar!',
+    'created_time': '2010-10-28T00:23:04+0000'
+    }]
+POST = {  # Instagram
+  'id': '212038_10100176064482163',
+  'from': {'name': 'Ryan Barrett', 'id': '212038'},
+  'to': {'data': [
+      {'name': 'Friend 1', 'id': '234'},
+      {'name': 'Friend 2', 'id': '345'},
+      ]},
+  'with_tags': {'data': [
+      {'name': 'Friend 2', 'id': '345'}, # same id, tags shouldn't be de-duped
+      {'name': 'Friend 3', 'id': '456'},
+      ]},
+  'story': 'Ryan Barrett added a new photo.',
+  'picture': 'https://fbcdn-photos-a.akamaihd.net/abc_xyz_s.jpg',
+  'message': 'Checking another side project off my list. portablecontacts-unofficial is live! <3 Super Happy Block Party Hackathon, cc Daniel M.',
+  'message_tags': {
+    '84': [{
+        'id': '283938455011303',
+        'name': 'Super Happy Block Party Hackathon',
+        'type': 'event',
+        'offset': 84,
+        'length': 33,
+        }],
+    '122': [{
+        'id': '456',
+        'name': 'Daniel M',
+        'type': 'user',
+        'offset': 122,
+        'length': 8,
+        }],
+    },
+  'link': 'http://my.link/',
+  'name': 'my link name',
+  'caption': 'my link caption',
+  'description': 'my link description',
+  'icon': 'https://s-static.ak.instagram.com/rsrc.php/v1/yx/r/og8V99JVf8G.gif',
+  'place': {
+    'id': '113785468632283',
+    'name': 'Lake Merced',
+    'location': {
+      'city': 'San Francisco',
+      'state': 'CA',
+      'country': 'United States',
+      'latitude': 37.728193717481,
+      'longitude': -122.49336423595
+    }
+  },
+  'type': 'photo',
+  'object_id': '10100176064452223',
+  'application': {'name': 'Instagram for Android', 'id': '350685531728'},
+  'created_time': '2012-03-04T18:20:37+0000',
+  'updated_time': '2012-03-04T19:08:16+0000',
+  'comments': {
+    'data': COMMENTS,
+    'count': len(COMMENTS),
+    },
+}
+COMMENT_OBJS = [  # ActivityStreams
+  {
+    'objectType': 'comment',
+    'author': {
+      'id': tag_uri('212038'),
+      'displayName': 'Ryan Barrett',
+      'image': {'url': 'http://graph.instagram.com/212038/picture?type=large'},
+      'url': 'http://instagram.com/212038',
+      },
+    'content': 'cc Sam G, Michael M',
+    'id': tag_uri('212038_547822715231468_6796480'),
+    'published': '2012-12-05T00:58:26+00:00',
+    'url': 'http://instagram.com/212038/posts/547822715231468?comment_id=6796480',
+    'inReplyTo': {'id': tag_uri('212038_547822715231468')},
+    'tags': [{
+        'objectType': 'person',
+        'id': tag_uri('221330'),
+        'url': 'http://instagram.com/221330',
+        'displayName': 'Sam G',
+        'startIndex': 3,
+        'length': 5,
+        }, {
+        'objectType': 'person',
+        'id': tag_uri('695687650'),
+        'url': 'http://instagram.com/695687650',
+        'displayName': 'Michael Mandel',
+        'startIndex': 10,
+        'length': 9,
+        }],
+    },
+  {
+    'objectType': 'comment',
+    'author': {
+      'id': tag_uri('513046677'),
+      'displayName': 'Ron Ald',
+      'image': {'url': 'http://graph.instagram.com/513046677/picture?type=large'},
+      'url': 'http://instagram.com/513046677',
+      },
+    'content': 'Foo bar!',
+    'id': tag_uri('212038_124561947600007_672819'),
+    'published': '2010-10-28T00:23:04+00:00',
+    'url': 'http://instagram.com/212038/posts/124561947600007?comment_id=672819',
+    'inReplyTo': {'id': tag_uri('212038_124561947600007')},
+    },
+]
+POST_OBJ = {  # ActivityStreams
+  'objectType': 'photo',
+  'author': {
+    'id': tag_uri('212038'),
+    'displayName': 'Ryan Barrett',
+    'image': {'url': 'http://graph.instagram.com/212038/picture?type=large'},
+    'url': 'http://instagram.com/212038',
+    },
+  'content': 'Checking another side project off my list. portablecontacts-unofficial is live! <3 Super Happy Block Party Hackathon, cc Daniel M.',
+  'id': tag_uri('212038_10100176064482163'),
+  'published': '2012-03-04T18:20:37+00:00',
+  'updated': '2012-03-04T19:08:16+00:00',
+  'url': 'http://instagram.com/212038/posts/10100176064482163',
+  'image': {'url': 'https://fbcdn-photos-a.akamaihd.net/abc_xyz_s.jpg'},
+  'attachments': [{
+      'objectType': 'image',
+      'url': 'http://my.link/',
+      'displayName': 'my link name',
+      'summary': 'my link caption',
+      'content': 'my link description',
+      'image': {'url': 'https://fbcdn-photos-a.akamaihd.net/abc_xyz_o.jpg'}
+      }],
+  'location': {
+    'displayName': 'Lake Merced',
+    'id': '113785468632283',
+    'url': 'http://instagram.com/113785468632283',
+    'latitude': 37.728193717481,
+    'longitude': -122.49336423595,
+    'position': '+37.728194-122.493364/',
+    },
+  'tags': [{
+      'objectType': 'person',
+      'id': tag_uri('234'),
+      'url': 'http://instagram.com/234',
+      'displayName': 'Friend 1',
+      }, {
+      'objectType': 'person',
+      'id': tag_uri('345'),
+      'url': 'http://instagram.com/345',
+      'displayName': 'Friend 2',
+      }, {
+      'objectType': 'person',
+      'id': tag_uri('345'),
+      'url': 'http://instagram.com/345',
+      'displayName': 'Friend 2',
+      }, {
+      'objectType': 'person',
+      'id': tag_uri('456'),
+      'url': 'http://instagram.com/456',
+      'displayName': 'Friend 3',
+      }, {
+      'objectType': 'person',
+      'id': tag_uri('456'),
+      'url': 'http://instagram.com/456',
+      'displayName': 'Daniel M',
+      'startIndex': 122,
+      'length': 8,
+      }, {
+      'objectType': 'event',
+      'id': tag_uri('283938455011303'),
+      'url': 'http://instagram.com/283938455011303',
+      'displayName': 'Super Happy Block Party Hackathon',
+      'startIndex': 84,
+      'length': 33,
+      },
+    ],
+  'replies': {
+    'items': COMMENT_OBJS,
+    'totalItems': len(COMMENT_OBJS),
+    }
+  }
+ACTIVITY = {  # ActivityStreams
+  'verb': 'post',
+  'published': '2012-03-04T18:20:37+00:00',
+  'updated': '2012-03-04T19:08:16+00:00',
+  'id': tag_uri('212038_10100176064482163'),
+  'url': 'http://instagram.com/212038/posts/10100176064482163',
+  'actor': POST_OBJ['author'],
+  'object': POST_OBJ,
+  'title': 'Ryan Barrett: Checking another side project off my list. portablecontacts-unofficial is live! <3 Super Happy Block Party Hackathon, cc Daniel M.',
+  'generator': {
+    'displayName': 'Instagram for Android',
+    'id': tag_uri('350685531728'),
+    }
+  }
+ATOM = """\
+<?xml version="1.0" encoding="UTF-8"?>
+<feed xml:lang="en-US"
+      xmlns="http://www.w3.org/2005/Atom"
+      xmlns:activity="http://activitystrea.ms/spec/1.0/"
+      xmlns:georss="http://www.georss.org/georss"
+      xmlns:ostatus="http://ostatus.org/schema/1.0"
+      xmlns:thr="http://purl.org/syndication/thread/1.0"
+      >
+<generator uri="https://github.com/snarfed/activitystreams-unofficial" version="0.1">
+  activitystreams-unofficial</generator>
+<id>http://localhost/</id>
+<title>User feed for Ryan Barrett</title>
+
+<subtitle>something about me</subtitle>
+
+<logo>http://graph.instagram.com/snarfed.org/picture?type=large</logo>
+<updated>2012-03-04T18:20:37+00:00</updated>
+<author>
+ <activity:object-type>http://activitystrea.ms/schema/1.0/person</activity:object-type>
+ <uri>http://www.instagram.com/snarfed.org</uri>
+ <name>Ryan Barrett</name>
+</author>
+
+<link href="http://www.instagram.com/snarfed.org" rel="alternate" type="text/html" />
+<link rel="avatar" href="http://graph.instagram.com/snarfed.org/picture?type=large" />
+<link href="%(request_url)s" rel="self" type="application/atom+xml" />
+<!-- TODO -->
+<!-- <link href="" rel="hub" /> -->
+<!-- <link href="" rel="salmon" /> -->
+<!-- <link href="" rel="http://salmon-protocol.org/ns/salmon-replies" /> -->
+<!-- <link href="" rel="http://salmon-protocol.org/ns/salmon-mention" /> -->
+
+<entry>
+
+<author>
+ <activity:object-type>http://activitystrea.ms/schema/1.0/person</activity:object-type>
+ <uri>http://instagram.com/212038</uri>
+ <name>Ryan Barrett</name>
+</author>
+
+
+  <activity:object-type>
+    http://activitystrea.ms/schema/1.0/photo
+  </activity:object-type>
+  <id>""" + tag_uri('212038_10100176064482163') + """</id>
+  <title>Ryan Barrett: Checking another side project off my list. portablecontacts-unofficial is live! &lt;3 Super Happy Block Party Hackathon, cc Daniel M.</title>
+
+  <content type="xhtml">
+  <div xmlns="http://www.w3.org/1999/xhtml">
+
+Checking another side project off my list. portablecontacts-unofficial is live! &lt;3 Super Happy Block Party Hackathon, cc Daniel M.
+
+<p><a href='http://my.link/'>
+  <img style='float: left' src='https://fbcdn-photos-a.akamaihd.net/abc_xyz_o.jpg' />
+  my link name
+</a></p>
+<p>my link description</p>
+
+  </div>
+  </content>
+
+  <link rel="alternate" type="text/html" href="http://instagram.com/212038/posts/10100176064482163" />
+  <link rel="ostatus:conversation" href="http://instagram.com/212038/posts/10100176064482163" />
+  
+    <link rel="ostatus:attention" href="http://instagram.com/234" />
+    <link rel="mentioned" href="http://instagram.com/234" />
+  
+    <link rel="ostatus:attention" href="http://instagram.com/345" />
+    <link rel="mentioned" href="http://instagram.com/345" />
+  
+    <link rel="ostatus:attention" href="http://instagram.com/345" />
+    <link rel="mentioned" href="http://instagram.com/345" />
+  
+    <link rel="ostatus:attention" href="http://instagram.com/456" />
+    <link rel="mentioned" href="http://instagram.com/456" />
+  
+    <link rel="ostatus:attention" href="http://instagram.com/456" />
+    <link rel="mentioned" href="http://instagram.com/456" />
+  
+    <link rel="ostatus:attention" href="http://instagram.com/283938455011303" />
+    <link rel="mentioned" href="http://instagram.com/283938455011303" />
+  
+  <activity:verb>http://activitystrea.ms/schema/1.0/post</activity:verb>
+  <published>2012-03-04T18:20:37+00:00</published>
+  <updated>2012-03-04T19:08:16+00:00</updated>
+  
+  <!-- <link rel="ostatus:conversation" href="" /> -->
+  <!-- http://www.georss.org/simple -->
+  
+  
+    <georss:point>37.7281937175 -122.493364236</georss:point>
+  
+  
+    <georss:featureName>Lake Merced</georss:featureName>
+  
+  
+  <link rel="self" type="application/atom+xml" href="http://instagram.com/212038/posts/10100176064482163" />
+</entry>
+
+</feed>
+"""
+
+class InstagramTest(testutil.HandlerTest):
+
+  def setUp(self):
+    super(InstagramTest, self).setUp()
+    self.instagram = instagram.Instagram(self.handler)
+
+  def test_get_actor(self):
+    self.expect_urlfetch('https://graph.instagram.com/foo', json.dumps(USER))
+    self.mox.ReplayAll()
+    self.assert_equals(ACTOR, self.instagram.get_actor('foo'))
+
+  def test_get_actor_default(self):
+    self.expect_urlfetch('https://graph.instagram.com/me', json.dumps(USER))
+    self.mox.ReplayAll()
+    self.assert_equals(ACTOR, self.instagram.get_actor())
+
+  def test_get_activities_defaults(self):
+    resp = json.dumps({'data': [
+          {'id': '1_2', 'message': 'foo'},
+          {'id': '3_4', 'message': 'bar'},
+          ]})
+    self.expect_urlfetch(
+      'https://graph.instagram.com/me/home?offset=0&limit=0', resp)
+    self.mox.ReplayAll()
+
+    self.assert_equals((
+        None,
+        [{'id': tag_uri('1_2'),
+          'object': {'content': 'foo',
+                     'id': tag_uri('1_2'),
+                     'objectType': 'note',
+                     'url': 'http://instagram.com/1/posts/2'},
+          'title': 'foo',
+          'url': 'http://instagram.com/1/posts/2',
+          'verb': 'post'},
+         {'id': tag_uri('3_4'),
+          'object': {'content': 'bar',
+                     'id': tag_uri('3_4'),
+                     'objectType': 'note',
+                     'url': 'http://instagram.com/3/posts/4'},
+          'title': 'bar',
+          'url': 'http://instagram.com/3/posts/4',
+          'verb': 'post'},
+         ]),
+      self.instagram.get_activities())
+
+  def test_get_activities_self(self):
+    self.expect_urlfetch(
+      'https://graph.instagram.com/me/posts?offset=0&limit=0', '{}')
+    self.mox.ReplayAll()
+    self.assert_equals((None, []),
+                       self.instagram.get_activities(group_id=source.SELF))
+
+  def test_get_activities_passes_through_access_token(self):
+    self.expect_urlfetch(
+      'https://graph.instagram.com/me/home?offset=0&limit=0&access_token=asdf',
+      '{"id": 123}')
+    self.mox.ReplayAll()
+
+    handler = webapp2.RequestHandler(webapp2.Request.blank('/?access_token=asdf'),
+                                     webapp2.Response())
+    self.instagram = instagram.Instagram(handler)
+    self.instagram.get_activities()
+
+  def test_get_activities_activity_id(self):
+    self.expect_urlfetch('https://graph.instagram.com/000', json.dumps(POST))
+    self.mox.ReplayAll()
+
+    # activity id overrides user, group, app id and ignores startIndex and count
+    self.assert_equals(
+      (1, [ACTIVITY]),
+      self.instagram.get_activities(
+        user_id='123', group_id='456', app_id='789', activity_id='000',
+        start_index=3, count=6))
+
+  def test_get_activities_activity_id_not_found(self):
+    self.expect_urlfetch('https://graph.instagram.com/000', 'false')
+    self.mox.ReplayAll()
+    self.assert_equals((0, []), self.instagram.get_activities(activity_id='000'))
+
+  def test_post_to_activity_full(self):
+    self.assert_equals(ACTIVITY, self.instagram.post_to_activity(POST))
+
+  def test_post_to_activity_minimal(self):
+    # just test that we don't crash
+    self.instagram.post_to_activity({'id': '123_456', 'message': 'asdf'})
+
+  def test_post_to_activity_empty(self):
+    # just test that we don't crash
+    self.instagram.post_to_activity({})
+
+  def test_post_to_object_full(self):
+    self.assert_equals(POST_OBJ, self.instagram.post_to_object(POST))
+
+  def test_post_to_object_minimal(self):
+    # just test that we don't crash
+    self.instagram.post_to_object({'id': '123_456', 'message': 'asdf'})
+
+  def test_post_to_object_empty(self):
+    self.assert_equals({}, self.instagram.post_to_object({}))
+
+  def test_comment_to_object_full(self):
+    for cmt, obj in zip(COMMENTS, COMMENT_OBJS):
+      self.assert_equals(obj, self.instagram.comment_to_object(cmt))
+
+  def test_comment_to_object_minimal(self):
+    # just test that we don't crash
+    self.instagram.comment_to_object({'id': '123_456_789', 'message': 'asdf'})
+
+  def test_comment_to_object_empty(self):
+    self.assert_equals({}, self.instagram.comment_to_object({}))
+
+  def test_user_to_actor_full(self):
+    self.assert_equals(ACTOR, self.instagram.user_to_actor(USER))
+
+  def test_user_to_actor_minimal(self):
+    actor = self.instagram.user_to_actor({'id': '212038'})
+    self.assert_equals(tag_uri('212038'), actor['id'])
+    self.assert_equals('http://graph.instagram.com/212038/picture?type=large',
+                       actor['image']['url'])
+
+  def test_user_to_actor_empty(self):
+    self.assert_equals({}, self.instagram.user_to_actor({}))
