883a26c967eaad3a993fdabaca86a0111017a307
Ryan Barrett <git@ryanb.org>
2013-10-16 22:10:54 -0700

    port facebook and twitter to oauth-dropins

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-#!/usr/bin/env python
-"""Serves the HTML front page and discovery files.
+"""Serves the the front page, discovery files, and OAuth flows.
 """
 
 __author__ = 'Ryan Barrett <activitystreams@ryanb.org>'
+
+import urllib
 
 import activitystreams
 import appengine_config
 import webapp2
 from webutil import handlers
+
+from oauth_dropins import facebook
+from oauth_dropins import twitter
+site_module = {'facebook-activitystreams': facebook,
+               'twitter-activitystreams': twitter,
+               }[appengine_config.APP_ID]
 
 
 class FrontPageHandler(handlers.TemplateHandler):
@@ -22,6 +29,19 @@ class FrontPageHandler(handlers.TemplateHandler):
             }
 
 
-application = webapp2.WSGIApplication(
-  [('/', FrontPageHandler)] + handlers.HOST_META_ROUTES,
+class CallbackHandler(site_module.CallbackHandler):
+  def finish(self, auth_entity, state=None):
+    return_params = {'facebook-activitystreams': ('access_token',),
+                     'twitter-activitystreams': ('token_key', 'token_secret'),
+                     }[appengine_config.APP_ID]
+
+    self.redirect('/?%s' % urllib.urlencode(
+        {k: getattr(auth_entity, k) for k in return_params}))
+
+
+application = webapp2.WSGIApplication([
+    ('/', FrontPageHandler),
+    ('/start_auth', site_module.StartHandler.to('/oauth_callback')),
+    ('/oauth_callback', CallbackHandler),
+    ] + handlers.HOST_META_ROUTES,
   debug=appengine_config.DEBUG)
