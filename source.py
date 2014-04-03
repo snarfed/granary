snarfed/granary

commit bf812140573dc9cd7c38e2aed63ddc043abf5f11

original post discovery heuristic: ellipsized URLs are probably incomplete, so omit them

source.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+# coding=utf-8
 """Source base class.
 
 Based on the OpenSocial ActivityStreams REST API:
@@ -379,8 +379,10 @@ class Source(object):
       if http not in existing and https not in existing:
         urls.add(http)
 
-    obj.setdefault('tags', []).extend({'objectType': 'article', 'url': u}
-                                      for u in urls)
+    obj.setdefault('tags', []).extend(
+      {'objectType': 'article', 'url': u} for u in urls
+      # heuristic: ellipsized URLs are probably incomplete, so omit them.
+      if not u.endswith('...') and not u.endswith('…'))
     return activity
 
   @staticmethod

source_test.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 """Unit tests for source.py.
 """
 
@@ -117,6 +118,16 @@ class SourceTest(testutil.HandlerTest):
         source.Source.original_post_discovery(activity)
         self.assert_equals([{'objectType': 'article', 'url': url}],
                            activity['object']['tags'])
+
+    # exclude ellipsized URLs
+    for ellipsis in '...', '…':
+      activity = {'object': {
+          'content': 'x (foo.com/1%s)' % ellipsis,
+          'attachments': [{'objectType': 'article',
+                           'url': 'http://foo.com/1%s' % ellipsis}],
+          }}
+      source.Source.original_post_discovery(activity)
+      self.assert_equals([], activity['object']['tags'])
 
   def test_get_like(self):
     self.source.get_activities(user_id='author', activity_id='activity',

commit 08d898db99a9a0fa34697871aa0ebb20b4497797

fix character encoding bug introduced in bf812140573dc9cd7c38e2aed63ddc043abf5f11

source.py
@@ -382,7 +382,7 @@ class Source(object):
     obj.setdefault('tags', []).extend(
       {'objectType': 'article', 'url': u} for u in urls
       # heuristic: ellipsized URLs are probably incomplete, so omit them.
-      if not u.endswith('...') and not u.endswith('…'))
+      if not u.endswith('...') and not u.endswith(u'…'))
     return activity
 
   @staticmethod

source_test.py
@@ -120,11 +120,11 @@ class SourceTest(testutil.HandlerTest):
                            activity['object']['tags'])
 
     # exclude ellipsized URLs
-    for ellipsis in '...', '…':
+    for ellipsis in '...', u'…':
+      url = 'foo.com/1' + ellipsis
       activity = {'object': {
-          'content': 'x (foo.com/1%s)' % ellipsis,
-          'attachments': [{'objectType': 'article',
-                           'url': 'http://foo.com/1%s' % ellipsis}],
+          'content': 'x (%s)' % url,
+          'attachments': [{'objectType': 'article', 'url': 'http://' + url}],
           }}
       source.Source.original_post_discovery(activity)
       self.assert_equals([], activity['object']['tags'])