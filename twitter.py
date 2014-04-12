71d985a0029a5aaed580d9dc7af423edbf4b0748
Kartik Prabhu <katsdaway@gmail.com>
2014-04-12 15:14:38 -0500

    regex bug fix
    fixed regex error in timezone stuff in ```rfc2822_to_iso8601```

--- twitter.py
+++ twitter.py
@@ -840,7 +840,7 @@ class Twitter(source.Source):
       return None
 
     without_timezone = re.sub(' [+-][0-9]{4} ', ' ', time_str)
-    timezone = re.search(' [+-][0-9]{4} ', time_str).group(1)
+    timezone = re.search('[+-][0-9]{4}', time_str).group(0)
     ## convert offset to seconds
     offset = 3600*int(timezone[1:3])+60*int(timezone[3:])
     ## negative offset
