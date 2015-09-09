d69b38deecf5afb8c43bc1f80d0cdbe03af275fb
Ryan Barrett <git@ryanb.org>
2015-09-08 20:58:41 -0700

    move bridgy.util.follow_redirects() to granary.source
    for snarfed/bridgy#51

--- granary/source.py
+++ granary/source.py
@@ -15,9 +15,13 @@ __author__ = ['Ryan Barrett <granary@ryanb.org>']
 import collections
 import copy
 import logging
+import mimetypes
 import re
 import urlparse
 
+import requests
+
+import appengine_config
 from oauth_dropins.webutil import util
 
 ME = '@me'
@@ -33,6 +37,8 @@ RSVP_TO_EVENT = {
   'rsvp-maybe': 'maybeAttending',
   'invite': 'invited',
   }
+
+FAILED_RESOLVE_URL_CACHE_TIME = 60 * 60 * 24  # a day
 
 # maps lower case string short name to Source subclass. populated by SourceMeta.
 sources = {}
@@ -695,3 +701,63 @@ class Source(object):
 
     ret = summary or content or name
     return ret.strip() if ret else None
+
+
+def follow_redirects(url, cache=None, **kwargs):
+  """Fetches a URL with HEAD, repeating if necessary to follow redirects.
+
+  Caches resolved URLs in memcache by default. *Does not* raise an exception if
+  any of the HTTP requests fail, just returns the failed response. If you care,
+  be sure to check the returned response's status code!
+
+  Args:
+    url: string
+    cache: optional, a cache object to read and write resolved URLs to. Must
+      have get(key) and set(key, value, time=...) methods. Stores
+      'R [original URL]' in key, final URL in value.
+    **kwargs: passed to requests.head()
+
+  Returns:
+    the requests.Response for the final request
+  """
+  if cache is not None:
+    cache_key = 'R ' + url
+    resolved = cache.get(cache_key)
+    if resolved is not None:
+      return resolved
+
+  # can't use urllib2 since it uses GET on redirect requests, even if i specify
+  # HEAD for the initial request.
+  # http://stackoverflow.com/questions/9967632
+  try:
+    # default scheme to http
+    parsed = urlparse.urlparse(url)
+    if not parsed.scheme:
+      url = 'http://' + url
+    kwargs.setdefault('timeout', appengine_config.HTTP_TIMEOUT)
+    resolved = requests.head(url, allow_redirects=True, **kwargs)
+    resolved.raise_for_status()
+    cache_time = 0  # forever
+  except AssertionError:
+    raise
+  except BaseException, e:
+    logging.warning("Couldn't resolve URL %s : %s", url, e)
+    resolved = requests.Response()
+    resolved.url = url
+    resolved.status_code = 499  # not standard. i made this up.
+    cache_time = FAILED_RESOLVE_URL_CACHE_TIME
+
+  content_type = resolved.headers.get('content-type')
+  if not content_type:
+    type, _ = mimetypes.guess_type(resolved.url)
+    resolved.headers['content-type'] = type or 'text/html'
+
+  refresh = resolved.headers.get('refresh')
+  if refresh:
+    for part in refresh.split(';'):
+      if part.strip().startswith('url='):
+        return follow_redirects(part.strip()[4:])
+
+  if cache is not None:
+    cache.set(cache_key, resolved, time=cache_time)
+  return resolved
--- granary/test/test_source.py
+++ granary/test/test_source.py
@@ -13,6 +13,7 @@ from granary import source
 from granary.source import Source
 from granary import testutil
 from granary import twitter
+from oauth_dropins.webutil import util
 
 import test_facebook
 import test_googleplus
@@ -74,7 +75,7 @@ class FakeSource(Source):
     pass
 
 
-class SourceTest(testutil.HandlerTest):
+class SourceTest(testutil.TestCase):
 
   def setUp(self):
     super(SourceTest, self).setUp()
@@ -298,3 +299,40 @@ class SourceTest(testutil.HandlerTest):
     self.assertEquals(instagram.Instagram, source.sources['instagram'])
     self.assertEquals(twitter.Twitter, source.sources['twitter'])
 
+  def test_follow_redirects(self):
+    for i in range(2):
+      self.expect_requests_head('http://will/redirect',
+                                redirected_url='http://final/url')
+    self.mox.ReplayAll()
+
+    cache = util.CacheDict()
+    self.assert_equals(
+      'http://final/url',
+      source.follow_redirects('http://will/redirect', cache=cache).url)
+
+    self.assertEquals('http://final/url', cache['R http://will/redirect'].url)
+
+    # another call without cache should refetch
+    self.assert_equals(
+      'http://final/url',
+      source.follow_redirects('http://will/redirect').url)
+
+    # another call with cache shouldn't refetch
+    self.assert_equals(
+      'http://final/url',
+      source.follow_redirects('http://will/redirect', cache=cache).url)
+
+
+  def test_follow_redirects_with_refresh_header(self):
+    self.expect_requests_head('http://will/redirect',
+                              response_headers={'refresh': '0; url=http://refresh'})
+    self.expect_requests_head('http://refresh', redirected_url='http://final')
+
+    self.mox.ReplayAll()
+    self.assert_equals('http://final',
+                       source.follow_redirects('http://will/redirect').url)
+
+  def test_follow_redirects_defaults_scheme_to_http(self):
+    self.expect_requests_head('http://foo/bar', redirected_url='http://final')
+    self.mox.ReplayAll()
+    self.assert_equals('http://final', source.follow_redirects('foo/bar').url)
