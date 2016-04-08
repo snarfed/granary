31d7944e580a07bcf21ff7a254c3b29a8fc32c5d
Ryan Barrett <git@ryanb.org>
2016-04-07 18:17:29 -0700

    drop ereporter in favor of stackdriver error reporting
    snarfed/bridgy#645
    https://cloudplatform.googleblog.com/2016/04/monitor-your-application-errors-with-Stackdriver-Error-Reporting.html
    https://cloud.google.com/error-reporting/

--- granary/appengine_config.py
+++ granary/appengine_config.py
@@ -4,9 +4,3 @@ from oauth_dropins.appengine_config import *
 # specifying one explicitly.
 import warnings
 warnings.filterwarnings('ignore', module='bs4', category=UserWarning)
-
-# Additional ereporter exceptions to suppress.
-ereporter_logging_handler.BLACKLIST += (
-  'HTTPError: HTTP Error 401: Unauthorized',
-  'HTTPError: HTTP Error 403: Forbidden',
-)
