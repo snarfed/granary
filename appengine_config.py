54654f4f06f031d9bb710637613da9f02489cd15
Ryan Barrett <git@ryanb.org>
2016-05-01 17:02:23 -0700

    add requests-toolbelt app engine monkeypatch to appengine_config.py
    ...since snarfed/oauth-dropins@99a754b4cd7b02b7d9485bde4429d473d725d4ee removed it from oauth-dropin's lib appengine_config.py.
    
    snarfed/bridgy#396

--- appengine_config.py
+++ appengine_config.py
@@ -4,3 +4,9 @@ from google.appengine.ext import vendor
 vendor.add('local')
 
 from granary.appengine_config import *
+
+# Make requests and urllib3 play nice with App Engine.
+# https://github.com/snarfed/bridgy/issues/396
+# http://stackoverflow.com/questions/34574740
+from requests_toolbelt.adapters import appengine
+appengine.monkeypatch()
