d0d3776e4861a371bf01cfdf9caf8232a5f7796e
Ryan Barrett <git@ryanb.org>
2017-07-26 09:46:44 -0700

    add instagram live test and cron job to trigger nightly circle build
    for #106

---
+++ cron.py
@@ -0,0 +1,19 @@
+"""Cron jobs. Currently just nightly CircleCI build."""
+
+import appengine_config
+
+import requests
+import webapp2
+
+CIRCLECI_TOKEN = appengine_config.read('circleci_token')
+
+
+class BuildCircle(webapp2.RequestHandler):
+  def get(self):
+    resp = requests.post('https://circleci.com/api/v1.1/project/github/snarfed/granary/tree/master?circle-token=%s' % CIRCLECI_TOKEN)
+    resp.raise_for_status()
+
+
+application = webapp2.WSGIApplication([
+  ('/cron/build_circle', BuildCircle),
+], debug=appengine_config.DEBUG)
