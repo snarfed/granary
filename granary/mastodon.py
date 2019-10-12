513695e01ef3b6b7569c072afe2ba8eff73cbbfc
Ryan Barrett <git@ryanb.org>
2019-10-12 15:45:17 -0700

    mastodon: start to add create and preview

--- granary/mastodon.py
+++ granary/mastodon.py
@@ -17,6 +17,8 @@ from . import appengine_config
 from . import as2
 from . import source
 
+API_STATUSES = '/api/v1/statuses'
+
 
 class Mastodon(source.Source):
   """Mastodon source class. See file docstring and Source class for details.
@@ -28,6 +30,10 @@ class Mastodon(source.Source):
   DOMAIN = 'N/A'
   BASE_URL = 'N/A'
   NAME = 'Mastodon'
+
+  # https://docs.joinmastodon.org/usage/basics/#text
+  TRUNCATE_TEXT_LENGTH = 500
+  TRUNCATE_URL_LENGTH = 23
 
   def __init__(self, instance, username=None, access_token=None):
     """Constructor.
@@ -44,6 +50,19 @@ class Mastodon(source.Source):
 
   def user_url(self, username):
     return urllib.parse.urljoin(self.instance, '@' + username)
+
+  def api(self, path, *args, **kwargs):
+    headers = kwargs.setdefault('headers', {})
+    headers['Authorization'] = 'Bearer ' + self.access_token
+
+    url = urllib.parse.urljoin(self.instance, path)
+    resp = util.requests_post(url, *args, **kwargs)
+    try:
+      resp.raise_for_status()
+    except BaseException as e:
+      util.interpret_http_exception(e)
+      raise
+    return resp
 
   def get_activities_response(self, user_id=None, group_id=None, app_id=None,
                               activity_id=None, fetch_replies=False,
@@ -68,3 +87,240 @@ class Mastodon(source.Source):
 
     response = self.make_activities_base_response(util.trim_nulls(activities))
     return response
+
+  def create(self, obj, include_link=source.OMIT_LINK,
+             ignore_formatting=False):
+    """Creates a status (aka toot), reply, boost (aka reblog), or favorite.
+
+    https://docs.joinmastodon.org/api/rest/statuses/
+
+    Args:
+      obj: ActivityStreams object
+      include_link: string
+      ignore_formatting: boolean
+
+    Returns: CreationResult whose content will be a dict with 'id', 'url', and
+      'type' keys (all optional) for the newly created object (or None)
+    """
+    return self._create(obj, preview=False, include_link=include_link,
+                        ignore_formatting=ignore_formatting)
+
+  def preview_create(self, obj, include_link=source.OMIT_LINK,
+                     ignore_formatting=False):
+    """Preview creating a status (aka toot), reply, boost (aka reblog), or favorite.
+
+    https://docs.joinmastodon.org/api/rest/statuses/
+
+    Args:
+      obj: ActivityStreams object
+      include_link: string
+      ignore_formatting: boolean
+
+    Returns: CreationResult whose content will be a unicode string HTML
+      snippet (or None)
+    """
+    return self._create(obj, preview=True, include_link=include_link,
+                        ignore_formatting=ignore_formatting)
+
+  def _create(self, obj, preview=None, include_link=source.OMIT_LINK,
+              ignore_formatting=False):
+    """Creates or previews a status (aka toot), reply, boost (aka reblog), or favorite.
+
+    https://docs.joinmastodon.org/api/rest/statuses/
+
+    Args:
+      obj: ActivityStreams object
+      preview: boolean
+      include_link: string
+      ignore_formatting: boolean
+
+    Returns: CreationResult. If preview is True, the content will be a unicode
+      string HTML snippet. If False, it will be a dict with 'id' and 'url' keys
+      for the newly created object.
+    """
+    assert preview in (False, True)
+    type = obj.get('objectType')
+    verb = obj.get('verb')
+
+    base_obj = self.base_object(obj)
+    base_id = base_obj.get('id')
+    base_url = base_obj.get('url')
+
+    is_reply = type == 'comment' or 'inReplyTo' in obj
+    is_rsvp = (verb and verb.startswith('rsvp-')) or verb == 'invite'
+    # images = util.get_list(obj, 'image')
+    # video_url = util.get_first(obj, 'stream', {}).get('url')
+    # has_media = (images or video_url) and (type in ('note', 'article') or is_reply)
+
+    # prefer displayName over content for articles
+    type = obj.get('objectType')
+    prefer_content = type == 'note' or (base_url and (type == 'comment'
+                                                      or obj.get('inReplyTo')))
+    preview_description = ''
+    quote_tweet_url = None
+#     for att in obj.get('attachments', []):
+#       url = self.URL_CANONICALIZER(att.get('url', ''))
+#       if url and TWEET_URL_RE.match(url):
+#         quote_tweet_url = url
+#         preview_description += """\
+# <span class="verb">quote</span>
+# <a href="%s">this tweet</a>:<br>
+# %s
+# <br>and """ % (url, self.embed_post(att))
+#         break
+
+    content = self._content_for_create(
+      obj, ignore_formatting=ignore_formatting, prefer_name=not prefer_content)
+      # strip_first_video_tag=bool(video_url), strip_quotations=bool(quote_tweet_url))
+
+    if not content:
+      if type == 'activity' and not is_rsvp:
+        content = verb
+      # elif has_media:
+      #   content = ''
+      else:
+        return source.creation_result(
+          abort=False,  # keep looking for things to publish,
+          error_plain='No content text found.',
+          error_html='No content text found.')
+
+#     if is_reply and base_url:
+#       # Twitter *used* to require replies to include an @-mention of the
+#       # original tweet's author
+#       # https://dev.twitter.com/docs/api/1.1/post/statuses/update#api-param-in_reply_to_status_id
+#       # ...but now we use the auto_populate_reply_metadata query param instead:
+#       # https://dev.twitter.com/overview/api/upcoming-changes-to-tweets
+
+#       # the embed URL in the preview can't start with mobile. or www., so just
+#       # hard-code it to twitter.com. index #1 is netloc.
+#       parsed = urllib.parse.urlparse(base_url)
+#       parts = parsed.path.split('/')
+#       if len(parts) < 2 or not parts[1]:
+#         raise ValueError('Could not determine author of in-reply-to URL %s' % base_url)
+#       reply_to_prefix = '@%s ' % parts[1].lower()
+#       if content.lower().startswith(reply_to_prefix):
+#         content = content[len(reply_to_prefix):]
+
+#       parsed = list(parsed)
+#       parsed[1] = self.DOMAIN
+#       base_url = urllib.parse.urlunparse(parsed)
+
+#     # need a base_url with the tweet id for the embed HTML below. do this
+#     # *after* checking the real base_url for in-reply-to author username.
+#     if base_id and not base_url:
+#       base_url = 'https://twitter.com/-/statuses/' + base_id
+
+#     if is_reply and not base_url:
+#       return source.creation_result(
+#         abort=True,
+#         error_plain='Could not find a tweet to reply to.',
+#         error_html='Could not find a tweet to <a href="http://indiewebcamp.com/reply">reply to</a>. '
+#         'Check that your post has an <a href="http://indiewebcamp.com/comment">in-reply-to</a> '
+#         'link a Twitter URL or to an original post that publishes a '
+#         '<a href="http://indiewebcamp.com/rel-syndication">rel-syndication</a> link to Twitter.')
+
+    # truncate and ellipsize content if it's over the character
+    # count. URLs will be t.co-wrapped, so include that when counting.
+    content = self.truncate(content, obj.get('url'), include_link, type)
+                             # quote_tweet=quote_tweet_url)
+
+    # linkify defaults to Twitter's link shortening behavior
+    preview_content = util.linkify(content, pretty=True, skip_bare_cc_tlds=True)
+    # TODO
+    # preview_content = MENTION_RE.sub(
+    #   r'\1<a href="https://twitter.com/\2">@\2</a>', preview_content)
+    # preview_content = HASHTAG_RE.sub(
+    #   r'\1<a href="https://twitter.com/hashtag/\2">#\2</a>', preview_content)
+
+#     if type == 'activity' and verb == 'like':
+#       if not base_url:
+#         return source.creation_result(
+#           abort=True,
+#           error_plain='Could not find a tweet to like.',
+#           error_html='Could not find a tweet to <a href="http://indiewebcamp.com/favorite">favorite</a>. '
+#           'Check that your post has a like-of link to a Twitter URL or to an original post that publishes a '
+#           '<a href="http://indiewebcamp.com/rel-syndication">rel-syndication</a> link to Twitter.')
+
+#       if preview:
+#         preview_description += """\
+# <span class="verb">favorite</span>
+# <a href="%s">this tweet</a>:
+# %s""" % (base_url, self.embed_post(base_obj))
+#         return source.creation_result(description=preview_description)
+#       else:
+#         data = urllib.parse.urlencode({'id': base_id})
+#         self.urlopen(API_POST_FAVORITE, data=data)
+#         resp = {'type': 'like'}
+
+#     elif type == 'activity' and verb == 'share':
+#       if not base_url:
+#         return source.creation_result(
+#           abort=True,
+#           error_plain='Could not find a tweet to retweet.',
+#           error_html='Could not find a tweet to <a href="http://indiewebcamp.com/repost">retweet</a>. '
+#           'Check that your post has a repost-of link to a Twitter URL or to an original post that publishes a '
+#           '<a href="http://indiewebcamp.com/rel-syndication">rel-syndication</a> link to Twitter.')
+
+#       if preview:
+#           preview_description += """\
+# <span class="verb">retweet</span>
+# <a href="%s">this tweet</a>:
+# %s""" % (base_url, self.embed_post(base_obj))
+#           return source.creation_result(description=preview_description)
+#       else:
+#         data = urllib.parse.urlencode({'id': base_id})
+#         resp = self.urlopen(API_POST_RETWEET % base_id, data=data)
+#         resp['type'] = 'repost'
+
+    if type in ('note', 'article') or is_reply or is_rsvp:  # a post
+      data = {'status': content}
+
+      if is_reply:
+        preview_description += """\
+<span class="verb">@-reply</span> to <a href="%s">this toot</a>:
+%s""" % (base_url, self.embed_post(base_obj))
+        data['in_reply_to_status_id'] = base_id
+      else:
+        preview_description += '<span class="verb">toot</span>:'
+
+#       if video_url:
+#         preview_content += ('<br /><br /><video controls src="%s"><a href="%s">'
+#                             'this video</a></video>' % (video_url, video_url))
+#         if not preview:
+#           ret = self.upload_video(video_url)
+#           if isinstance(ret, source.CreationResult):
+#             return ret
+#           data.append(('media_ids', ret))
+
+#       elif images:
+#         num = len(images)
+#         if num > MAX_MEDIA:
+#           images = images[:MAX_MEDIA]
+#           logging.warning('Found %d photos! Only using the first %d: %r',
+#                           num, MAX_MEDIA, images)
+#         preview_content += '<br /><br />' + ' &nbsp; '.join(
+#           '<img src="%s" alt="%s" />' % (img.get('url'), img.get('displayName', ''))
+#                                          for img in images)
+#         if not preview:
+#           ret = self.upload_images(images)
+#           if isinstance(ret, source.CreationResult):
+#             return ret
+#           data.append(('media_ids', ','.join(ret)))
+
+      if preview:
+        return source.creation_result(content=preview_content,
+                                      description=preview_description)
+      else:
+        resp = json.loads(self.api(API_STATUSES, json=data).text)
+        resp['type'] = 'comment' if is_reply else 'post'
+
+    else:
+      return source.creation_result(
+        abort=False,
+        error_plain='Cannot publish type=%s, verb=%s to Mastodon' % (type, verb),
+        error_html='Cannot publish type=%s, verb=%s to Mastodon' % (type, verb))
+
+    if 'url' not in resp:
+      resp['url'] = base_url
+
+    return source.creation_result(resp)
--- granary/tests/test_mastodon.py
+++ granary/tests/test_mastodon.py
@@ -1,18 +1,97 @@
 # coding=utf-8
-"""Unit tests for mastodon.py.
-"""
+"""Unit tests for mastodon.py."""
+from __future__ import unicode_literals
+from future import standard_library
+standard_library.install_aliases()
+
+import copy
+
 from oauth_dropins.webutil import testutil
 import ujson as json
 
 from granary import appengine_config
 from granary import as2, mastodon
+from granary.mastodon import (
+  API_STATUSES,
+)
 
+INSTANCE = 'http://foo.com'
+
+# Mastodon
+# https://docs.joinmastodon.org/api/entities/#account
+ACCOUNT = {
+  'id': '23507',
+  'username': 'snarfed',
+  'acct': 'snarfed',  # fully qualified if on a different instance
+  'url': 'https://mastodon.technology/@snarfed',
+  'display_name': 'Ryan Barrett',
+  'avatar': 'https://cdn.mastodon.technology/accounts/avatars/000/023/507/original/01401e3558e03feb.png',
+  'avatar_static': 'https://cdn.mastodon.technology/accounts/avatars/000/023/507/original/01401e3558e03feb.png',
+  'header': 'https://cdn.mastodon.technology/accounts/headers/000/023/507/original/977013e803161a99.jpg',
+  'header_static': 'https://cdn.mastodon.technology/accounts/headers/000/023/507/original/977013e803161a99.jpg',
+  'note': '<p></p>',
+  'created_at': '2017-04-19T20:38:19.704Z',
+  'statuses_count': 19,
+  'following_count': 12,
+  'followers_count': 64,
+  'fields': [{
+    'verified_at': '2019-04-03T17:32:24.467+00:00',
+    'name': 'Web site',
+    'value': '<a href=\'https://snarfed.org\' rel=\'me nofollow noopener\' target=\'_blank\'><span class=\'invisible\'>https://</span><span class=\'\'>snarfed.org</span><span class=\'invisible\'></span></a>'
+  }],
+  'emojis': [],
+  'bot': False,
+  'locked': False,
+}
+
+# https://docs.joinmastodon.org/api/entities/#status
+STATUS = {
+  'id': '102526179954060294',
+  'url': 'https://mastodon.technology/@snarfed/102526179954060294',
+  'uri': 'https://mastodon.technology/users/snarfed/statuses/102526179954060294',
+  'account': ACCOUNT,
+  'in_reply_to_id': '102482741701856532',
+  'in_reply_to_account_id': '11018',
+  'content': '<p><span class=\'h-card\'><a href=\'https://queer.party/@fluffy\' class=\'u-url mention\'>@<span>fluffy</span></a></span> <span class=\'h-card\'><a href=\'https://mastodon.lubar.me/@ben\' class=\'u-url mention\'>@<span>ben</span></a></span> <span class=\'h-card\'><a href=\'https://fed.brid.gy/r/http://beesbuzz.biz/\' class=\'u-url mention\'>@<span>beesbuzz.biz</span></a></span> nope, no recent bridgy fed changes. it definitely does make the fediverse think your site is an activitypub/mastodon instance, which will trigger requests like these, but that’s always been true. more details: <a href=\'https://github.com/snarfed/bridgy-fed/issues/38\' rel=\'nofollow noopener\' target=\'_blank\'><span class=\'invisible\'>https://</span><span class=\'ellipsis\'>github.com/snarfed/bridgy-fed/</span><span class=\'invisible\'>issues/38</span></a></p>',
+  'mentions': [{
+    'id': '11018',
+    'username': 'fluffy',
+    'acct': 'fluffy@queer.party',
+    'url': 'https://queer.party/@fluffy',
+  }],
+  'created_at': '2019-07-29T18:35:53.446Z',
+  'media_attachments': [],
+  'replies_count': 1,
+  'favourites_count': 0,
+  'reblogs_count': 0,
+  'visibility': 'public',
+  'spoiler_text': '',
+  'card': {'...'},
+  'application': {'website': None, 'name': 'Web'},
+  'poll': None,
+  'language': 'en',
+  'emojis': [],
+  'tags': [],
+  'reblog': None,
+  'muted': False,
+  'reblogged': False,
+  'favourited': False,
+  'sensitive': False,
+  'pinned': False,
+}
 
 class MastodonTest(testutil.TestCase):
 
   def setUp(self):
     super(MastodonTest, self).setUp()
-    self.mastodon = mastodon.Mastodon('http://foo.com', username='alice')
+    self.mastodon = mastodon.Mastodon(INSTANCE, username='alice',
+                                      access_token='towkin')
+
+  def expect_api(self, path, response=None, **kwargs):
+    kwargs.setdefault('headers', {}).update({
+      'Authorization': 'Bearer towkin',
+    })
+    return self.expect_requests_post(INSTANCE + path, response=response, **kwargs)
 
   def test_get_activities_defaults(self):
     self.expect_requests_get('http://foo.com/users/alice/outbox?page=true', json.dumps({
@@ -26,3 +105,28 @@ class MastodonTest(testutil.TestCase):
       {'content': 'foo bar'},
       {'content': 'bar baz'},
     ], self.mastodon.get_activities())
+
+  def test_create_status(self):
+    self.expect_api(API_STATUSES, json={
+      'status': 'foo ☕ bar'
+    }, response=STATUS)
+    self.mox.ReplayAll()
+
+    result = self.mastodon.create({
+      'objectType': 'note',
+      'content': 'foo ☕ bar',
+    })
+
+    expected = copy.deepcopy(STATUS)
+    expected['type'] = 'post'
+    self.assert_equals(expected, result.content, result)
+    self.assertIsNone(result.error_plain)
+    self.assertIsNone(result.error_html)
+
+  def test_preview_status(self):
+    got = self.mastodon.preview_create({
+      'objectType': 'note',
+      'content': 'foo ☕ bar',
+    })
+    self.assertEqual('<span class="verb">toot</span>:', got.description)
+    self.assertEqual('foo ☕ bar', got.content)
