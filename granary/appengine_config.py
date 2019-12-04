6030a049c126cb82713013666480d943332716b8
Ryan Barrett <git@ryanb.org>
2019-12-04 07:32:11 -0800

    python 3: drop requirements.freeze.txt
    relax to just approximate pinning, roughly based on semver. allow up to latest minor and patch versions, pin major version to avoid breaking changes.
    
    background:
    
    https://www.python.org/dev/peps/pep-0440/#compatible-release
    https://medium.com/@jimjh/managing-dependencies-in-python-applications-b9c93dda98c2

--- granary/appengine_config.py
+++ granary/appengine_config.py
@@ -5,7 +5,6 @@ from oauth_dropins.appengine_config import *
 import warnings
 warnings.filterwarnings('ignore', module='bs4',
                         message='No parser was explicitly specified')
-warnings.filterwarnings('ignore', message='urllib3 is using URLFetch')
 warnings.filterwarnings('ignore',
                         message='URLFetch does not support granular timeout')
 warnings.filterwarnings('ignore',
