ffbda02f587840ca908075d6974ebab0e35cbb6f
Ryan Barrett <git@ryanb.org>
2021-05-04 22:56:12 -0700

    master => main (git branch)

--- cron.py
+++ cron.py
@@ -9,5 +9,5 @@ CIRCLECI_TOKEN = util.read('circleci_token')
 
 class BuildCircle(webapp2.RequestHandler):
   def get(self):
-    resp = requests.post('https://circleci.com/api/v1.1/project/github/snarfed/granary/tree/master?circle-token=%s' % CIRCLECI_TOKEN)
+    resp = requests.post('https://circleci.com/api/v1.1/project/github/snarfed/granary/tree/main?circle-token=%s' % CIRCLECI_TOKEN)
     resp.raise_for_status()
