27cfc74123e894a8a797b6626d21f57a4c50ea43
Ryan Barrett <git@ryanb.org>
2021-08-31 20:48:57 -0700

    Flask config: drop unnecessary github client id/secret files

--- config.py
+++ config.py
@@ -9,12 +9,8 @@ JSONIFY_PRETTYPRINT_REGULAR = True
 if appengine_info.DEBUG:
   ENV = 'development'
   CACHE_TYPE = 'NullCache'
-  GITHUB_CLIENT_ID = util.read('github_client_id_local')
-  GITHUB_CLIENT_SECRET = util.read('github_client_secret_local')
   SECRET_KEY = 'sooper seekret'
 else:
   ENV = 'production'
   CACHE_TYPE = 'SimpleCache'
-  GITHUB_CLIENT_ID = util.read('github_client_id')
-  GITHUB_CLIENT_SECRET = util.read('github_client_secret')
   SECRET_KEY = util.read('flask_secret_key')
