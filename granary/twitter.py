fc81f2ec2be6c2f73f6e5a7dd2610f93a994a820
Ryan Barrett <git@ryanb.org>
2022-12-04 23:14:36 -0800

    crashes

--- granary/twitter.py
+++ granary/twitter.py
@@ -1,12 +1,11 @@
 class Foo():
 
   def __init__(self):
-    self.foo = 3
+    pass
 
-  def get_activities_response(self):
+  def bar(self):
     """Fetches posts and converts them to ActivityStreams activities.
     See :meth:`source.Source.get_activities_response()` for details. app_id is
     ignored. min_id is translated to Twitter's since_id.
     """
-    if foo and bar:
-        raise baz
+    pass
