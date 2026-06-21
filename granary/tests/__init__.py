fa3827ddf92bd8b811dcc07190635590f80e79f7
Ryan Barrett <git@ryanb.org>
2026-06-21 09:13:27 -0700

    tests/__init__.py: stop reusing webutil's

--- granary/tests/__init__.py
+++ granary/tests/__init__.py
@@ -1,3 +1,16 @@
-# webutil/tests/__init__.py has setup code that makes App Engine SDK's
-# bundled libraries importable.
-import webutil.tests
+import io, logging, os
+
+from webutil.appengine_info import DEBUG
+
+assert DEBUG
+creds = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+assert not creds or creds.endswith('fake_user_account.json')
+
+import sys
+logging.basicConfig()
+if '-v' in sys.argv:
+    logging.getLogger().setLevel(logging.DEBUG)
+else:
+    handler = logging.getLogger().handlers[0]
+    if hasattr(handler, 'setStream'):
+        handler.setStream(io.StringIO())
